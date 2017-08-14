c7eb7097dbf658364f14e94c194e0ee5e9ea3445
Jing Gao <tracygj1990@gmail.com>
2017-08-14 13:45:15 +1000

    first commit

--- myCaculator/ViewController.swift
+++ myCaculator/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping: Bool
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textCurrentlyInDisplay = display!.text!
+        display!.text = textCurrentlyInDisplay + digit
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
